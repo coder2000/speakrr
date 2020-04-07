7baff456d277ffb47566593ba192c42e8ff9e2de
Dieter Lunn <coder2000@gmail.com>
2020-04-07 18:02:21 -0500

    feat: add webpack config

---
+++ client/webpack.dev.ts
@@ -0,0 +1,29 @@
+import * as webpack from "webpack";
+
+const config: webpack.Configuration = {
+  entry: "./src/index.tsx",
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/env"] },
+      },
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        loader: "awesome-typescript-loader",
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: { extensions: ["*", ".js", ".jsx", ".ts", ".tsx"] },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+};
+
+export default config;
---
+++ client/webpack.prod.ts
@@ -0,0 +1,28 @@
+import * as webpack from "webpack";
+
+const config: webpack.Configuration = {
+  entry: "./src/index.tsx",
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/env"] },
+      },
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        loader: "awesome-typescript-loader",
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: { extensions: ["*", ".js", ".jsx", ".ts", ".tsx"] },
+};
+
+export default config;
