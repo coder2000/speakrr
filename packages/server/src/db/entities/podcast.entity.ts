af1805f51c32891190d139cdad2a0eb72ae5c27f
Dieter Lunn <coder2000@gmail.com>
2020-04-08 13:40:03 -0500

    feat: add graphql object types to podcast

--- packages/server/src/db/entities/podcast.entity.ts
+++ packages/server/src/db/entities/podcast.entity.ts
@@ -6,31 +6,40 @@ import {
   OneToMany,
   PrimaryGeneratedColumn,
 } from 'typeorm';
+import { Field, ObjectType } from '@nestjs/graphql';
 import { Author } from './author.entity';
 import { Episode } from './episode.entity';
 import { Category } from './category.entity';
 
 @Entity()
+@ObjectType()
 export class Podcast {
   @PrimaryGeneratedColumn()
+  @Field()
   id: number;
 
   @Column()
+  @Field()
   title: string;
 
   @Column()
+  @Field()
   description: string;
 
   @Column()
+  @Field()
   image: string;
 
   @Column()
+  @Field()
   language: string;
 
   @Column()
+  @Field()
   link: string;
 
   @Column()
+  @Field()
   explicit: boolean;
 
   @ManyToOne((type) => Author, (author) => author.podcasts)
