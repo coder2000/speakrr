43f95e592cd93bdc40ddb5ff758d35bd921ed39b
Dieter Lunn <coder2000@gmail.com>
2020-05-09 15:17:17 -0500

    refactor: change queryservice decorator according to new release

--- packages/server/src/modules/queue/queue.resolver.ts
+++ packages/server/src/modules/queue/queue.resolver.ts
@@ -1,7 +1,7 @@
 import { Resolver } from '@nestjs/graphql';
 import { CRUDResolver } from '@nestjs-query/query-graphql';
-import { QueryService } from '@nestjs-query/core';
-import { InjectTypeOrmQueryService } from '@nestjs-query/query-typeorm';
+import { InjectQueryService, QueryService } from '@nestjs-query/core';
+
 import { QueueEntity } from '@entities/queue.entity';
 
 @Resolver(() => QueueEntity)
@@ -9,7 +9,7 @@ export class QueueResolver extends CRUDResolver(QueueEntity, {
   update: { disabled: true },
 }) {
   constructor(
-    @InjectTypeOrmQueryService(QueueEntity)
+    @InjectQueryService(QueueEntity)
     readonly queueService: QueryService<QueueEntity>,
   ) {
     super(queueService);
